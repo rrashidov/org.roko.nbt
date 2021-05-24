fb62026a4e79769b87103a5b652e8fe7408594d3
Rashid Rashidov <rrashidov@gmail.com>
2021-05-24 18:24:33 +0300

    Introduce distributed persistence module

---
+++ org.roko.nbt.persistence.distrib/src/main/java/org/roko/nbt/persistence/distrib/DistributedPersistenceService.java
@@ -0,0 +1,14 @@
+package org.roko.nbt.persistence.distrib;
+
+import org.roko.nbt.domain.api.DomainEntity;
+import org.roko.nbt.persistence.api.PersistenceService;
+
+public class DistributedPersistenceService implements PersistenceService {
+
+	@Override
+	public DomainEntity save(DomainEntity domainEntity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
